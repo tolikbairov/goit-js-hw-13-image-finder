91ba59d335f39a731bd9a9f49d9b8a1359adb8c7
Anatolii Bairov <tolikbairov16@gmail.com>
2021-09-09 16:41:16 +0300

    async

---
+++ src/js/asyncApiService.js
@@ -0,0 +1,26 @@
+export default class PictureApiService {
+  BASE_URL = 'https://pixabay.com/api';
+  API_KEY = '23305129-dc426c4ab0f279a7253128e3a';
+  searchQuery = '';
+  page = 1;
+  async fetchPictures() {
+    const requestParams = `image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.API_KEY}`;
+    const url = `${this.BASE_URL}/?${requestParams}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    this.incrementPage();
+    return data.hits;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(string) {
+    this.searchQuery = string;
+  }
+}
